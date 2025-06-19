fcd5e23ca360ad736297602d08b350bb04024acf
Junn34 <lucasorjh3412@gmail.com>
2025-06-19 14:42:15 +0900

    <골드4> 스택에 대한 이해가 아직 부족

--- 코딩테스트/src/스택과큐/P17298_오큰수구하기.java
+++ 코딩테스트/src/스택과큐/P17298_오큰수구하기.java
@@ -2,41 +2,41 @@ package 스택과큐;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class P17298_오큰수구하기 {
+public class P17298_오큰수구하기 {//top 보다 index에 있는 값이 더크면 pop 연산 실행 후 top에 있던 pop한 인덱스에 오큰수 저장ㄴ 
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st=new StringTokenizer(br.readLine());
-		int N=Integer.parseInt(st.nextToken());
-		int[] arr=new int[N];
-		st=new StringTokenizer(br.readLine());
-		for(int i=0;i<N;i++) {
-			arr[i]=Integer.parseInt(st.nextToken());
+		int n=Integer.parseInt(br.readLine());
+		int[] A=new int[n];
+		int[] ans=new int[n];
+		String[] str=br.readLine().split(" ");
+		for(int i=0;i<n;i++) {
+			A[i]=Integer.parseInt(str[i]);
 		}
-		//알고리즘
-		int[] result=new int[N];
-		result[N-1]=-1;
-		for(int i=0;i<N-1;i++) {
-			ArrayList<Integer> al=new ArrayList<>();
-			
-			for(int j=i+1;j<N;j++) {
-				if(arr[i]<arr[j]) {
-					al.add(arr[j]);
-					break;
-				}
-				
+		Stack<Integer> myStack= new Stack<>();
+		myStack.push(0);//처음엔 항상 스택이 비어있으므로 최초값 push해 초기화
+		for(int i=1;i<n;i++) {
+			//스택이 비어있지않고 현재 수열이 스택 top 인덱스가 가리키는 수열보다 클 경우
+			while(!myStack.isEmpty()&&A[myStack.peek()]<A[i]) {
+				ans[myStack.pop()]=A[i];//정답 배열에 오큰수를 현재 수열로 저장
 			}
-			
-			if(al.size()==0) result[i]=-1;
-			else result[i]=al.get(0);
-			
+			myStack.push(i);	//신규 데이터 push
 		}
-		for(int i=0;i<result.length;i++) {
-			System.out.print(result[i]+" ");
-			
+		while(!myStack.empty()){
+			//반복문 다 돌고 나왔는데 스택이 비어있지않다면 빌 때까지
+			ans[myStack.pop()]=-1;
+			//스택에 쌓인 index에 1을 넣고
 		}
+		BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+		for(int i=0;i<n;i++) {
+			bw.write(ans[i]+" ");
+		}//출력
+		bw.write("\n");
+		bw.flush();
+		
 		
 	}
 
