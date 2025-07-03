0b0317d7ef6e8483c0480f28bc82b8c7ed189850
배준혁 <lucasorjh3412@gmail.com>
2025-07-03 10:06:33 +0900

    트리의 지름 bfs 끝.. 여전히 모름

--- 코딩테스트/src/탐색_BFS/P1167_트리지름구하기.java
+++ 코딩테스트/src/탐색_BFS/P1167_트리지름구하기.java
@@ -15,7 +15,7 @@ public class P1167_트리지름구하기 {
 		for(int i=1;i<=N;i++) {
 			A[i]=new ArrayList<Edge>();
 		}
-		for(int i=0;i<N;i++) {
+		for(int i=0;i<N;i++) {//A 인접 리스트에 그래프 데이터 저장
 			int S=sc.nextInt();
 			while(true){
 				int E=sc.nextInt();
@@ -28,14 +28,41 @@ public class P1167_트리지름구하기 {
 		visited=new boolean[N+1];
 		BFS(1);
 		int Max =1;
-		for(int i=2;i<=N;i++) {
+		for(int i=2;i<=N;i++) { //distance 배열에서 가장 큰 값으로 다시 시작점 설정
 			if(distance[Max]<distance[i])
 				Max=i;
 		}
+		distance=new int[N+1];
+		visited=new boolean[N+1];
+		BFS(Max);
+		Arrays.sort(distance);
+		System.out.println(distance[N]);
 	}
-	private static void BFS(ArrayList<Integer>[] a2) {
+	private static void BFS(int index) {
 		// TODO Auto-generated method stub
-		
+		Queue<Integer> queue = new LinkedList<Integer>();
+		queue.add(index);
+		visited[index]=true;
+		while(!queue.isEmpty()) {
+			int now_node = queue.poll();
+			for(Edge i:A[now_node]) {
+				int e=i.e;
+				int v=i.value;
+				if(!visited[e]) {
+					visited[e]=true;
+					queue.add(e);
+					distance[e]=distance[now_node]+v;//거리 배열 업데이트
+				}
+			}
+		}
 	}
 
+}
+class Edge{
+	int e;
+	int value;
+	public Edge(int e,int value) {
+		this.e=e;
+		this.value=value;
+	}
 }
