e906e1beb8999d82b249666746f6151591e1cadf
junn34 <lucasorjh3412@gmail.com>
2025-08-08 23:51:21 +0900

    분노의 dfsbfs

---
+++ 코딩테스트/src/정렬_병합정렬/국영수.java
@@ -0,0 +1,22 @@
+package 정렬_병합정렬;
+
+public class 국영수 {
+	
+	static class Student{
+		String name;
+		int kor;
+		int eng;
+		int math;
+		Student(String name,int kor,int eng,int math){
+			this.name=name;
+			this.eng=eng;
+			this.kor=kor;
+			this.math=math;
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- 코딩테스트/src/탐색_BFS/P1260_DFS와BFS.java
+++ 코딩테스트/src/탐색_BFS/P1260_DFS와BFS.java
@@ -3,66 +3,60 @@ package 탐색_BFS;
 import java.util.*;
 import java.io.*;
 public class P1260_DFS와BFS {
-	static ArrayList<Integer>[] A;
 	static boolean[] visited;
-	static int N;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st=new StringTokenizer(br.readLine());
-		N=Integer.parseInt(st.nextToken());
-		int M=Integer.parseInt(st.nextToken());
-		int V=Integer.parseInt(st.nextToken());
-		A=new ArrayList[N+1];
-		visited=new boolean[N+1];
-		for(int i=1;i<N+1;i++) {
-			A[i]=new ArrayList<Integer>();
-		}
-		for(int i=1;i<M+1;i++) {
-			st=new StringTokenizer(br.readLine());
-			int s=Integer.parseInt(st.nextToken());
-			int e=Integer.parseInt(st.nextToken());
-			A[s].add(e);
-			A[e].add(s);
-		}
+	  static ArrayList<Integer>[] al;
+	public static void main(String[] args)  throws IOException{
+		  BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		  StringTokenizer st=new StringTokenizer(br.readLine());
+		  int N=Integer.parseInt(st.nextToken());
+		  int M=Integer.parseInt(st.nextToken());
+		  int V=Integer.parseInt(st.nextToken());
+		  visited=new boolean[N+1];
+		  al=new ArrayList[N+1];
+		  for(int i=1;i<N+1;i++){
+		    al[i]=new ArrayList<>();
+		  }
+		  for(int i=0;i<M;i++){
+		    st=new StringTokenizer(br.readLine());
+		    int s=Integer.parseInt(st.nextToken());
+		    int e=Integer.parseInt(st.nextToken());
+		    al[s].add(e);
+		    al[e].add(s);
+		  }
+		   for (int i = 1; i <= N; i++) {
+		      Collections.sort(al[i]);
+		    }
+		  DFS(V);
+		  System.out.println();
+		  visited=new boolean[N+1];
+		  BFS(V);
 
-		for(int i=1;i<=N;i++) {
-			Collections.sort(A[i]);
-		}
-		DFS(V);
-		System.out.println();
-		visited=new boolean[N+1];
-		BFS(V);
-		System.out.println();
-	}
-	
-	
-	private static void BFS(int Node) {
-		// TODO Auto-generated method stub
-		Queue<Integer> queue=new LinkedList<Integer>();
-		queue.add(Node);
-		visited[Node]=true;
-		
-		while(!queue.isEmpty()) {
-			int now_Node = queue.poll();
-			System.out.print(now_Node+" ");
-			for(int i:A[now_Node]) {
-				visited[i]=true;
-				queue.add(i);
-			}
-		}
-	}
+		  }
+		  public static void DFS(int t){
 
-
-	private static void DFS(int Node) {
-		System.out.print(Node+" ");
-		visited[Node]=true;
-		for(int i:A[Node]) {
-			if(!visited[i]) {
-				DFS(i);
-			}
-		}
-		
-	}
+		    System.out.print(t+" ");
+		    visited[t]=true;
+		    for(int i:al[t]){
+		      if(!visited[i])
+		        DFS(i);
+		    }
+		  }
+		  public static void BFS(int t){
+		    Queue<Integer> queue=new LinkedList<Integer>();
+		    visited[t]=true;
+		    queue.add(t);
+		    while(!queue.isEmpty()){
+		      int n=queue.poll();
+		      System.out.print(n+" ");
+		      for(int i:al[n]){
+		        if(!visited[i]){
+		          visited[i]=true;
+		          queue.add(i);
+		        }
+		        
+		      }
+		    }
+		  }
 	
 
 }
